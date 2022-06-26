208bb518bd863bfc703b357ed43457cbd06f7508
kennedy <kennedyeziechina@gmail.com>
2022-06-26 12:12:08 +0100

    dev commit

--- testing.py
+++ testing.py
@@ -35,9 +35,58 @@
 
 # from django.core.cache import cache
 
-def multipliers():
-    return [lambda x: i * x for i in range(4)]
+# def multipliers():
+#     return [lambda x: i * x for i in range(4)]
+#
+#
+# final = [m(2) for m in multipliers()]
+# print(final)
 
 
-final = [m(2) for m in multipliers()]
-print(final)
+# def solution(A):
+#     # write your code in Python 3.6
+#     multiples = [num for num in A if num % 4 == 0]
+#     result = sum(multiples)
+#     print(result)
+#
+#
+# solution([2,5,6,8,44,40,20])
+
+
+# def solution(A):
+#     # write your code in Python 3.6
+#     count = 0
+#     for i in range(len(A)):
+#         if i != len(A) - 1:
+#             if A[i] == A[i +1]:
+#                 if A[i + 1] == 0:
+#                     A[i + 1] = 1
+#                     count += 1
+#                 elif A[i + 1] == 1:
+#                     A[i + 1] = 0
+#                     count += 1
+#     print(A, count)
+#
+# solution([1,1,0,1,1])
+
+
+def solution(A):
+    if len(A) > 0:
+        count1 = 0
+        count2 = 0
+        for index in range(len(A)):
+            if index % 2 == 0:
+                if A[index] == 1:
+                    count1 += 1
+                if A[index] == 0:
+                    count2 += 1
+            else:
+                if A[index] == 0:
+                    count1 += 1
+                if A[index] == 1:
+                    count2 += 1
+        print(count1, count2)
+        return min(count1, count2)
+
+
+print(solution([1,1,0,1,1,1]))
